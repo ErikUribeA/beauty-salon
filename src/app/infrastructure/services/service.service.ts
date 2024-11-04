d2a37a7ab9a01c9397cf4399d12cf138aaed5060
Erik Uribe <erickjosueuribe@gmail.com>
2024-11-04 15:55:25 -0500

    commit

---
+++ src/app/infrastructure/services/service.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from "../utils/client-http";
+
+export class VacantService {
+    private httpClient: HttpClient;
+
+    constructor() {
+        this.httpClient = new HttpClient()
+    }
+
+    async findAll(page: number, size: number): Promise<> {
+        try {
+            const response = await this.httpClient.get<IResponseV>(`vacants?page=${page}&size=${size}`);
+
+            return response.content;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+/*     async destroy(id: string) {
+        try {
+            const coders = this.httpClient.delete<IVacant[]>(`Coders/${id}`)
+
+            return coders
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+    }
+
+    async create(vacant: ICreateVacancy) {
+        try {
+            const createCompany = this.httpClient.post("vacants", vacant)
+            return createCompany
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+    }
+
+    async save(id: string, body:ICreateVacancy) {
+        try {
+            const actualizarCoder = await this.httpClient.put(`vacants/${id}`, body);
+            return actualizarCoder;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    } */
+}
