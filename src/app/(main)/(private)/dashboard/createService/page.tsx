6814e2f096e6eb447953fe4c93b76452547d463b
Erik <erickjosueuribe@gmail.com>
2024-11-05 06:43:28 -0500

    fixed

--- src/app/(main)/(private)/dashboard/createService/page.tsx
+++ src/app/(main)/(private)/dashboard/createService/page.tsx
@@ -1,10 +1,9 @@
-import { PostServiceForm } from '@/UI/organims/newServiceForm'
 import React from 'react'
 
 export default function CreateNewService() {
     return (
         <div>
-            <PostServiceForm />
+            Create a new service
         </div>
     )
 }
